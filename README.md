e2ab4412e54632fd82fb86ba7a1091879b134b42
Michael Cronk <michaelcronk@Michaels-MacBook-Pro-6.local>
2018-08-24 10:08:11 -0400

    Pre-batch-processing backup

--- README.md
+++ README.md
@@ -6,14 +6,27 @@ Will automatically re-run on each save of preProcess.js
 dev: npm run dev
 
 check fileType
+  if(audioType)
   //check audioformat
-    //prompt user to adjust format.
+    -->prompt user to adjust format.
   //check audiosampling
-    //prompt user to adjust sampling
+    -->prompt user to adjust sampling
   //check filesize
-    //prompt user to split into pieces
+    -->prompt user to split into pieces
 
+  //prompt user to send to STT
+    -->STT options
+  //prompt user to send to translate
+    -->rest of pipeline
 
+not Audio?  END
+
+TODO:  
+Provide check command for individual checks (e.g. just sampling, just size, just filetype)
+Switch preprocess pipeline to run on directories of files 
+
+//FLAC if space permits for lossless.
+//MP3 if we can afford slight compression.
 
 CLI - Vorpal: https://github.com/dthree/vorpal
 Dev - Nodemon: https://nodemon.io/
--- preProcess.js
+++ preProcess.js
@@ -1,6 +1,9 @@
 
 const readChunk = require('read-chunk');
 const fileType = require('file-type');
+const ffmpeg = require('ffmpeg');
+const async = require('async');
+
 const vorpal = require('vorpal')();
 
 
@@ -21,12 +24,44 @@ var stt_content_types = [
       'audio/webm;codecs=vorbis'
 ];
 
+var stt_extra_processing = [
+      'audio/basic'             : 'Use only with narrowband models.',
+      'audio/flac',             : '',
+      'audio/l16',              : 'Specify the sampling rate (rate) and optionally the number of channels (channels) and endianness (endianness) of the audio.',
+      'audio/mp3',              : '',
+      'audio/mpeg',             : '',
+      'audio/mulaw',            : 'Specify the sampling rate (rate) of the audio.',
+      'audio/ogg',              : 'The service automatically detects the codec of the input audio.',
+      'audio/ogg;codecs=opus',  : '',
+      'audio/ogg;codecs=vorbis',: '',
+      'audio/wav',              : 'Provide audio with a maximum of nine channels.',
+      'audio/webm',             : 'The service automatically detects the codec of the input audio.',
+      'audio/webm;codecs=opus', : '',
+      'audio/webm;codecs=vorbis': '',
+];
+
+
+var fileMetadata = {
+  'Filename'  : '',
+  'MIMEType'  : '',
+  'Size'      : '',
+  'SampleRate': '',
+  'Channels'  : '',
+  'BitRate'   : '',
+  'Filename'  : '',
+  'Video'     : 'False',  //Was the audio originally part of a video
+  'Split'     : 'False',  //Was the audio split into smaller files
+  'Actions'   : []        //PreProcessor Actions
+
+}
+
 vorpal
   .command('process <filepath>', 'Processes [filepath].')
   .alias('p')
   .action(function(args, callback) {
-    console.log(args['filepath']);
-    getFileType(args['filepath'])
+    // console.log(args['filepath']);
+
+  preProcess(args);
 
 
 
@@ -47,15 +82,120 @@ vorpal
   .show();
 
 
+function preProcess(args) {
+  function returnArgs() {//temporary getter.
+    return args;
+  }
+
+  async.waterfall([
+      function processFiletype(callback) {
+        var args = returnArgs();
+        console.log(args);
+        var filePath = args['filepath'];
+
+
+
+//******DELETE ME AFTER DEV****************
+        filePath = '/Users/michaelcronk/Desktop/audio.mp3'
+
+        getFileType(filePath, function (fileType) {//Get filetype
+          if (stt_content_types.indexOf(fileType) > -1) {//If filetype is STT compatible, move to next stage
+            console.log("Compatible MIMEtype detected: " + fileType);
+            callback(null, filePath);
+          } else if (fileType.substring(0,5) == 'video') {//if filetype is a video, extract mp3 audio and move to next stage
+            console.log("Video MIMEtype detected: " + fileType);
+
+            extractAudio(filePath, function(err, newPath) {
+              if (err) {
+                callback(err);//if extractAudio returns an error, propogate it to the end of the waterfall.
+              }
+              console.log("Mp3 extracted at: " + newPath);
+              callback(null, newPath);
+            });//end extractAudio
+          } else {//if the filetype is not STT compatible or a video with audio we can extract, abort.
+            console.log("Incompatible filetype detected.  Aborting");
+            callback(new Error("Incompatible filetype detected: " + fileType));
+          }
+        });//end getFileType
+
+    }//end processFiletype
+
+  ], function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Pre-process results path: " + results);
+  });//end async.waterfall
+
+
+
+
+}
+
+
 /**
 
 Returns filetype based on file buffer magic number.
 https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files
 */
-function getFileType(filepath) {
+function getFileType(filepath, callback) {
   const buffer = readChunk.sync(filepath, 0, 4100);
-  console.log(fileType(buffer));
+  var type = fileType(buffer);
+  callback(type['mime']);
 };
+/**
+Extracts audio from video types
+https://www.npmjs.com/package/ffmpeg
+*/
+function extractAudio(filePath, callback) {
+  console.log("Beginning audio extraction.");
+  try {
+  	var process = new ffmpeg(filePath);
+  	process.then(function (video) {
+
+      var mp3Path = filePath.split('.').slice(0, -1).join('.') + 'mp3';//replaces extension wth mp3
+  		video.fnExtractSoundToMP3(mp3Path, function (error, file) {
+  			if (!error)
+  				console.log('Audio file: ' + mp3Path);
+          callback(null, mp3Path);
+  		});
+  	}, function (err) {
+      console.log('extractAudio fnExtractSoundToMP3 handler: Cannot extract audio, aborting.');
+  		callback(err);
+  	});
+  } catch (e) {
+    console.log('extractAudio catch: Cannot extract audio, aborting.');
+    callback(e);
+
+  }
+
+}
+
+
+
+
+
+
+//check sampling rates
+function checkSamplingRate(filepath) {
+
+//This is really tough
+
+}
+/**
+
+
+
+Changes sampling for Broadband/Narrowband
+**From audio format docs**
+    For broadband models, the service converts audio recorded at higher sampling rates to 16 kHz.
+    For narrowband models, it converts the audio to 8 kHz.
+*/
+function adjustSampling() {
+//https://github.com/xdissent/node-resampler
+
+}
+
 
 
 /**
@@ -78,37 +218,4 @@ splits audio into X sized chunks for STT processing
 function splitAudio() {
   return "";
 
-};
-
-/**
-
-
-
-Changes sampling for Broadband/Narrowband
-**From audio format docs**
-    For broadband models, the service converts audio recorded at higher sampling rates to 16 kHz.
-    For narrowband models, it converts the audio to 8 kHz.
-*/
-function adjustSampling() {
-//https://github.com/xdissent/node-resampler
-
-}
-
-/**
-
-Extracts audio from video types
-https://www.npmjs.com/package/ffmpeg
-*/
-function extractAudio() {
-
-
-}
-
-/**
-
-
-Switches audio to mp3 filetype.
-*/
-function changeAudio() {
-  return "";
 };
